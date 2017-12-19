9adac652359f52b83a80bfbda91e8a120fc2a8dc
leslie <hushuangsoutheastasia@gmail.com>
2017-12-19 09:46:33 +0800

    修改原项目  使能编译通过

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/SettingsActivity.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/SettingsActivity.kt
@@ -9,7 +9,7 @@ import android.preference.EditTextPreference
 import android.preference.Preference
 import android.preference.PreferenceFragment
 import com.v2ray.ang.BuildConfig
-import com.v2ray.ang.InappBuyActivity
+// import com.v2ray.ang.InappBuyActivity
 import com.v2ray.ang.R
 import com.v2ray.ang.extension.defaultDPreference
 import com.v2ray.ang.extension.onClick
@@ -114,7 +114,7 @@ class SettingsActivity : BaseActivity() {
         }
 
         private fun donate() {
-            startActivity<InappBuyActivity>()
+            // startActivity<InappBuyActivity>()
         }
     }
 
